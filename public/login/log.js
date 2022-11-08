ea71931cb8e629d22eb2c970cc11ed834c5053ae
Manish Madan <96079232+ManishMadan2882@users.noreply.github.com>
2022-11-09 00:43:09 +0530

    login page
    making post req to /login route

---
+++ public/login/log.js
@@ -0,0 +1,29 @@
+
+const btn = document.getElementById('btn');
+const username = document.getElementById('username');
+const password = document.getElementById('password');
+const msg = document.getElementById('msg');
+
+
+async function authUser(event){
+    event.preventDefault();
+  
+    const data = {
+      username : username.value,
+      password : password.value
+    } ;
+    console.log(data);
+    await fetch("/login", {
+        method: "POST",
+        headers: {'Content-Type': 'application/json'}, 
+        body: JSON.stringify(data)
+      })
+      .then(res => res.json())
+      .then((data) => {
+        if(data.status)
+        location.href = "/";
+        else
+        msg.innerText = data.message;
+      })  
+}
+btn.addEventListener('click',authUser);
