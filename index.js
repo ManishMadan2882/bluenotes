6dd4ae74abcafbd3f3c4cf52441489b4e7ddaf6b
Manish Madan <96079232+ManishMadan2882@users.noreply.github.com>
2022-11-18 05:15:35 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+require('dotenv').config();
+
+const port = Number(process.env.PORT) || 7000;
+
+const express = require('express');
+
+const path = require('path');
+
+const app = express();
+
+const routerFile = require('./src/routes/route');
+
+
+app.use('/',routerFile);
+
+
+app.listen(port , () => {
+
+    console.log(`Server running at port : ${port}`);
+
+});
